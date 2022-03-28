e8e676961db1ce7301219a3ff1e7d6d9bfe3195a
jimmychanshiji <jimmyfungchan@gmail.com>
2022-03-29 00:11:26 +0100

    day 1 build

---
+++ components/Footer.js
@@ -0,0 +1,40 @@
+function Footer() {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32 py-14 bg-gray-100 text-gray-600">
+        <div className="space-y-4 text-xs text-gray-800">
+            <h5 className="font-bold">ABOUT</h5>
+            <p>How Airbnb works</p>
+            <p>Newsroom</p>
+            <p>Investors</p>
+            <p>Airbnb Plus</p>
+            <p>Airbnb Luxe</p>
+        </div>
+        <div className="space-y-4 text-xs text-gray-800">
+            <h5 className="font-bold">COMMUNITY</h5>
+            <p>Accessibility</p>
+            <p>This is not a real site</p>
+            <p>Its a pretty awesome clone</p>
+            <p>Referrals accepted</p>
+            <p>Jimmhfy</p>
+        </div>
+        <div className="space-y-4 text-xs text-gray-800">
+            <h5 className="font-bold">HOST</h5>
+            <p>Lets see my Github</p>
+            <p>Learning Frontend</p>
+            <p>Fullstack is great</p>
+            <p>Software Engineer</p>
+            <p>Learning Frontend</p>
+        </div>
+        <div className="space-y-4 text-xs text-gray-800">
+            <h5 className="font-bold">Tech</h5>
+            <p>Lets do programming</p>
+            <p>Javascript</p>
+            <p>Node.js</p>
+            <p>TailwindCSS</p>
+            <p>Next.js</p>
+        </div>
+    </div>
+  )
+}
+
+export default Footer
